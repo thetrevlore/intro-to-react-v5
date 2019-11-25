3d3c3bcb403ae0e7cd6abca083cdd8ec9e30d163
Trevor Storey <trevor@photoshelter.com>
2019-11-24 22:05:34 -0700

    [master] useEffect in SearchParams

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,23 @@
-import React, { useState } from "react";
-import { ANIMALS } from "@frontendmasters/pet";
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
+
+  // useEffect does not run on first render
+  useEffect(() => {
+    setBreeds([]);
+    setBreed("");
+
+    pet.breeds(animal).then(({ breeds: apiBreeds }) => {
+      const breedStrs = apiBreeds.map(({ name }) => name);
+      setBreeds(breedStrs);
+    }, console.error);
+  }, [animal, setBreed, setBreeds]); // this effect will only run when one of these changes. if you give it an empty array, it will run only on first mount and if you give it no argument here, it will run every render.
 
   return (
     <div className="search-params">
